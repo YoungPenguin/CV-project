f51370ef52fa9ff5a263684e427e34eda67a018d
thomasveje <thomasveje@gmail.com>
2021-04-11 23:07:32 +0200

    oops. fix dimension mixup

--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,7 @@ import math
 def splitY(overlap, image):
     assert 0 <= overlap and overlap < 1, "overlapping area should be a positive percentage less than 1 in decimal notation"
     print(image.shape)
-    height = image.shape[1]
+    height = image.shape[0]
     top_end = math.ceil((height + height*overlap) / 2)
     bottom_start = height - top_end
     top = image[:top_end]
@@ -15,7 +15,7 @@ def splitY(overlap, image):
 def splitX(overlap, image):
     assert 0 < overlap and overlap < 1, "overlap should be a decimal between 0 and 1"
     print(image.shape)
-    width = image.shape[0]
+    width = image.shape[1]
     left_end = math.ceil((width + width*overlap) / 2)
     right_start = width - left_end
     left = image[:,:left_end]
