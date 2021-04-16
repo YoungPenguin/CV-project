31fbfb0e4d99e6835d95b6182256bd2472003392
thomasveje <thomasveje@gmail.com>
2021-04-16 15:05:48 +0200

    fix fejl

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,7 @@ def splitY(overlap, image):
     assert 0 <= overlap and overlap < 1, "overlapping area should be a positive percentage less than 1 in decimal notation"
     print(image.shape)
     height = image.shape[0]
-    top_end = math.ceil((height + height*overlap) / 2)
+    top_end = math.ceil((height + 0.5*height*overlap) / 2)
     bottom_start = height - top_end
     top = image[:top_end]
     bottom = image[bottom_start:]
@@ -16,7 +16,7 @@ def splitX(overlap, image):
     assert 0 < overlap and overlap < 1, "overlap should be a decimal between 0 and 1"
     print(image.shape)
     width = image.shape[1]
-    left_end = math.ceil((width + width*overlap) / 2)
+    left_end = math.ceil((width + 0.5*width*overlap) / 2)
     right_start = width - left_end
     left = image[:,:left_end]
     right = image[:,right_start:]
