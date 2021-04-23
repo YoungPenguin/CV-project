f0eca9393c7ac5b3fd9b7291546fcefe459906d2
ditteblom <82377296+ditteblom@users.noreply.github.com>
2021-04-23 16:29:58 +0200

    harris corners

--- harrisCorners.py
+++ harrisCorners.py
@@ -11,8 +11,9 @@ import numpy as np
 import preprocessing as pre
 import cv as cvfunctions
 
-#%%
-## Image preprocessing 
+######################################################
+#%% IMAGE PROCESSING
+######################################################
 # choose 1 image: NORMAL2-IM-0329-0001.jpeg
 # divide image into two parts with 30% overlap, use open cv
 
@@ -30,10 +31,13 @@ ax[0].imshow(top)
 ax[1].imshow(full)
 ax[2].imshow(bottom)
 
+######################################################
+#%% FEATURE EXTRACTION
+######################################################
 # find corners
 s = 15 # s = [15;50]
-eps = 1.0 # eps > 0.5
-k = 0.1 # k < 0.2
+eps = 1.3 # eps > 0.5
+k = 0.01 # k ~ 0.01
 tau = 0 # tau ~ 0 ([-0.5;0.5])
 ctop = cvfunctions.cornerDetector(topGray/255.0, s, eps, k, tau)
 cbot = cvfunctions.cornerDetector(bottomGray/255.0, s, eps, k, tau)
@@ -44,4 +48,41 @@ ax[0].imshow(topGray,cmap='gray')
 ax[0].scatter(ctop[0,:], ctop[1,:],s=25,c='r',marker='.')
 ax[1].imshow(bottomGray,cmap='gray')
 ax[1].scatter(cbot[0,:], cbot[1,:],s=25,c='r',marker='.')
+plt.show()
+
+######################################################
+#%% FEATURE DESCRIPTOR
+######################################################
+# Brief descriptor
+kps1, f1, kps2, f2 = cvfunctions.brief_descriptor(topGray, bottomGray, ctop, cbot)
+
+bf = cv2.BFMatcher(cv2.NORM_L2)
+matches = bf.knnMatch(f1,f2,k=2)
+# Apply ratio test
+good_match = []
+for m,n in matches:
+    if m.distance < 0.5*n.distance:
+        good_match.append(m)
+# Sort them in the order of their distance.
+
+# show matches
+img3 = cv2.drawMatches(topGray, kps1, bottomGray, kps2, good_match,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+plt.figure(figsize=(30,20))
+plt.imshow(img3),plt.show()
+
+######################################################
+#%% HOMOGRAPHY ESTIMATION
+######################################################
+# Find homography
+
+flattened = [val for sublist in matches for val in sublist]
+src_pts = np.float32([ kps1[m.queryIdx].pt for m in flattened ]).reshape(-1,1,2)
+dst_pts = np.float32([ kps2[m.trainIdx].pt for m in flattened ]).reshape(-1,1,2)
+H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+
+######################################################
+#%% IMAGE STITCHING
+######################################################
+output = cvfunctions.warpImages(bottomGray, topGray, H)
+plt.imshow(output)
 plt.show()
