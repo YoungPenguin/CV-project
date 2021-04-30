8e0503b824c5f2ecf50edf5aedf248ebb96ab710
thomasveje <thomasveje@gmail.com>
2021-04-30 13:48:36 +0200

    orb graphs in the end

--- ORB.py
+++ ORB.py
@@ -9,6 +9,7 @@ import numpy as np
 import preprocessing as pre
 import cv as cvfunctions
 from scipy import ndimage
+import time
 
 #%%
 ## Image preprocessing 
@@ -29,17 +30,7 @@ dim = (width, height)
 # resize image
 topGray = cv2.resize(topGray, dim, interpolation = cv2.INTER_AREA)
 
-fig, axs = plt.subplots(1, 3, figsize=(15,5))
-axs = axs.flatten()
-for img, ax in zip([topGray,full,bottomGray], axs):
-    ax.axes.get_xaxis().set_visible(False)
-    ax.axes.get_yaxis().set_visible(False)
-    ax.imshow(img,cmap='gray')
-fig.suptitle('Image split')
-axs[0].set_title('Top part')
-axs[1].set_title('Full image')
-axs[2].set_title('Bottom part')
-plt.show()
+start = time.time()
 
 #%% Feature extraction and description
 # ORB
@@ -54,6 +45,50 @@ kp2, des2 = orb.detectAndCompute(bottomGray, None)
 result_im1 = cv2.drawKeypoints(topGray, kp1, None, (255,0,0), flags=0)
 result_im2 = cv2.drawKeypoints(bottomGray, kp2, None,(255,0,0), flags=0)
 
+
+
+# Uing Brute Force matcher with Hamming distance
+# create BFMatcher object
+distance_method = cv2.NORM_HAMMING
+bf = cv2.BFMatcher(distance_method, crossCheck=True)
+
+# Match descriptors
+matches = bf.match(des1,des2)
+
+stop = time.time()
+
+print('Ran in ' + str(stop-start))
+
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+# Draw first 10 matches.
+
+#best_x_matches = len(matches)
+best_x_matches = 100
+img3 = cv2.drawMatches(topGray, kp1, bottomGray, kp2, matches[:best_x_matches],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+# %% Homography estimation and image stitching
+src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ])
+dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ])
+H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+
+output = cvfunctions.warpImages(bottomGray, topGray, H)
+
+# %% All the graphs
+
+
+fig, axs = plt.subplots(1, 3, figsize=(15,5))
+axs = axs.flatten()
+for img, ax in zip([topGray,full,bottomGray], axs):
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.imshow(img,cmap='gray')
+fig.suptitle('Image split')
+axs[0].set_title('Top part')
+axs[1].set_title('Full image')
+axs[2].set_title('Bottom part')
+plt.show()
+
 fig, axs = plt.subplots(1, 2, figsize=(15,4))
 axs = axs.flatten()
 for img, ax in zip([result_im1,result_im2], axs):
@@ -65,22 +100,6 @@ axs[0].set_title('Top')
 axs[1].set_title('Bottom part')
 plt.show()
 
-# Uing Brute Force matcher with Hamming distance
-# create BFMatcher object
-distance_method = cv2.NORM_HAMMING
-bf = cv2.BFMatcher(distance_method, crossCheck=True)
-
-# Match descriptors
-matches = bf.match(des1,des2)
-
-# Sort them in the order of their distance.
-matches = sorted(matches, key = lambda x:x.distance)
-# Draw first 10 matches.
-
-#best_x_matches = len(matches)
-best_x_matches = 100
-img3 = cv2.drawMatches(topGray, kp1, bottomGray, kp2, matches[:best_x_matches],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
 f, ax = plt.subplots(figsize=(15,5))
 ax.axes.get_xaxis().set_visible(False)
 ax.axes.get_yaxis().set_visible(False)
@@ -88,13 +107,6 @@ ax.imshow(img3)
 ax.set_title('Matched ORB keypoints (best ' + str(best_x_matches)+ ' using Hamming distance)')
 f.savefig('images/orb_matches.png',bbox_inches='tight')
 plt.show()
-
-# %% Homography estimation and image stitching
-src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ])
-dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ])
-H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-
-output = cvfunctions.warpImages(bottomGray, topGray, H)
 
 fig, axs = plt.subplots(1, 3, figsize=(15,5))
 axs = axs.flatten()
